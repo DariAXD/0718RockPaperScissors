8c96f1b72276888137155d381a29386b96b6220c
ThereseXD <35051370+ThereseXD@users.noreply.github.com>
2024-07-22 12:18:50 -0400

    Revisiting

--- javascript.js
+++ javascript.js
@@ -14,19 +14,19 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-// console.log(computerChoice);
+// // console.log(computerChoice);
 
-function getHumanChoice(){
-    let humanChoice = prompt(`What's your choice?`).toLowerCase();
+// // function getHumanChoice(){
+// //     let humanChoice = prompt(`What's your choice?`).toLowerCase();
 
-    while( !( humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors' )){
-        humanChoice = prompt (`Please enter the valid value among 'rock' 'paper''scissors'`).toLowerCase();
-    } 
+// //     while( !( humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors' )){
+// //         humanChoice = prompt (`Please enter the valid value among 'rock' 'paper''scissors'`).toLowerCase();
+// //     } 
         
-    return humanChoice;
-}
+// //     return humanChoice;
+// // }
 
-// console.log(humanChoice);
+// // console.log(humanChoice);
 
 let humanScore = 0;
 let computerScore = 0;
@@ -35,41 +35,78 @@ function playRound(humanSide,computerSide){
     // win situations
     if ((humanSide ==='rock'&& computerSide ==='scissors') || (humanSide ==='paper'&& computerSide ==='rock') ||(humanSide==='scissors'&& computerSide ==='paper')) {
         humanScore += 1;
-        console.log (`You win! ${humanSide} beats ${computerSide}. Your Score: ${humanScore} Computer: ${computerScore} `)
+        let singleResult = document.createElement('li');
+        singleResult.textContent = `You win! ${humanSide} beats ${computerSide}. Your Score: ${humanScore} Computer: ${computerScore} `;
+        singleResultList.appendChild(singleResult);
         return;
     }  
     // lose situations
     else if ((humanSide ==='scissors'&& computerSide ==='rock') || (humanSide ==='rock'&& computerSide ==='paper') ||(humanSide==='paper'&& computerSide ==='scissors')){
         computerScore+=1;
-        console.log(`You lose! ${computerSide} beats ${humanSide}. Your Score: ${humanScore} Computer: ${computerScore} `);
+        let singleResult = document.createElement('li');
+        singleResult.textContent = `You lose! ${computerSide} beats ${humanSide}. Your Score: ${humanScore} Computer: ${computerScore} `;
+        singleResultList.appendChild(singleResult);
         return;
     }
     // tie situations 
     else { 
-        console.log(`It's a tie! ${computerSide} - ${humanSide}. Your Score: ${humanScore} Computer: ${computerScore} `)
+        let singleResult = document.createElement('li');
+        singleResult.textContent =`It's a tie! ${computerSide} - ${humanSide}. Your Score: ${humanScore} Computer: ${computerScore} `;
+        singleResultList.appendChild(singleResult);
         return;
     }
 
 }
 
 
-function playGame(){
-    for(i=0; i<5 ; i++){
-        const humanSide = getHumanChoice();
-        const computerSide = getComputerChoice();
-        playRound(humanSide,computerSide);
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`You win! Your Score: ${humanScore} Computer: ${computerScore} `);
-    } else if (humanScore < computerScore) {
-        console.log(`You lose! Your Score: ${humanScore} Computer: ${computerScore} `)
+function playGame(humanSide,computerSide){
+        // const humanSide = getHumanChoice();
+        // const computerSide = getComputerChoice();
+    if (humanScore === 5) {
+        finalResult.textContent = `You win! Your Score: ${humanScore} Computer: ${computerScore} `;
+    } else if (computerScore === 5) {
+        finalResult.textContent = `You lose! Your Score: ${humanScore} Computer: ${computerScore} `;
     } else {
-        console.log(`It's a tie! Your Score: ${humanScore} Computer: ${computerScore} `)
+        playRound(humanSide,computerSide);
     }
 
     return;
 
 }
 
-playGame()
+// playGame();
+
+let humanClick = document.querySelector('.humanClick');
+let humanSide = '';
+let singleResultList = document.querySelector('#singleResults');
+let finalResult = document.querySelector('#finalResult')
+
+humanClick.addEventListener('click', (event) => {
+    let target = event.target;
+    
+
+    switch(target.id){
+        case 'rock':
+            humanSide = 'rock';
+            break;
+
+        case 'paper':
+            humanSide = 'paper';
+            break;
+
+        case 'scissors':
+            humanSide = 'scissors'
+            break;
+    
+    }
+    
+let humanInput = document.querySelector('#humanInput');
+humanInput.textContent = `You choose ${humanSide}`;
+
+let computerSide = getComputerChoice();
+
+
+playGame(humanSide,computerSide);
+
+   
+});
